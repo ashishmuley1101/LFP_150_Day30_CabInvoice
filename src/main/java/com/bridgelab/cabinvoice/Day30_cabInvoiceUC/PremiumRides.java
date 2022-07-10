6e6cc225b28dff443074794112c5545d807efbaf
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-11 00:20:20 +0530

    Primium Rides

---
+++ src/main/java/com/bridgelab/cabinvoice/Day30_cabInvoiceUC/PremiumRides.java
@@ -0,0 +1,24 @@
+package com.bridgelab.cabinvoice.Day30_cabInvoiceUC;
+
+public class PremiumRides {
+
+	private final double PRE_COST_PER_KM = 20.0;
+    private final int PRE_COST_PER_MIN = 2;
+    private final double PRE_MIN_FARE = 20.0;
+    
+
+    
+    public double premiumCalculateFare(double distance, int time) {
+        double totalFare = distance * PRE_COST_PER_KM + time * PRE_COST_PER_MIN;
+        return Math.max(totalFare, PRE_MIN_FARE);
+    }
+    
+    public PremiumInvoice premiumCalculateFare(Rides[] rides) {
+        double totalFare = 0.0;
+        for (Rides ride : rides) {
+            totalFare += premiumCalculateFare(ride.getDistance(), ride.getTime());
+        }
+        System.out.println("length "+rides.length +" fare" +totalFare);
+        return new PremiumInvoice(rides.length, totalFare);
+    }
+}
