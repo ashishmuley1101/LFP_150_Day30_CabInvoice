5dba3237bdccab1bd0a973f3c6a8ba2023b41640
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-11 00:22:03 +0530

    switch to master

--- src/main/java/com/bridgelab/cabinvoice/Day30_cabInvoiceUC/PremiumRidesTest.java
+++ src/main/java/com/bridgelab/cabinvoice/Day30_cabInvoiceUC/PremiumRidesTest.java
@@ -13,11 +13,11 @@ public class PremiumRidesTest {
 		Assert.assertEquals(240.0, totalFare, 0.0);
 	}
 	
-//	@Test
-//	public void givenDistanceAndTime_whenPremiumCalculateFareIsLessThan20_ShouldReturnTotalFareAs5() {
-//		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
-//		double totalFare = invoiceService.calculateFare(5.0, 5);
-//		System.out.println("Total Fare = " + totalFare);
-//		Assert.assertEquals(55, totalFare, 0.0);
-//	}
+	@Test
+	public void givenDistanceAndTime_whenPremiumCalculateFareIsLessThan20_ShouldReturnTotalFareAs5() {
+		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+		double totalFare = invoiceService.calculateFare(5.0, 5);
+		System.out.println("Total Fare = " + totalFare);
+		Assert.assertEquals(55, totalFare, 0.0);
+	}
 }
