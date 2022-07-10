6383b0692cca9492cbc0017a3b2d2f55a326d9a0
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-10 23:41:03 +0530

    Calculate fare less than 5

--- src/main/java/com/bridgelab/cabinvoice/Day30_cabInvoice/InvoiceServiceTest.java
+++ src/main/java/com/bridgelab/cabinvoice/Day30_cabInvoice/InvoiceServiceTest.java
@@ -6,10 +6,26 @@ import org.junit.Test;
 public class InvoiceServiceTest {
 
 	@Test
-    public void givenDistanceAndTime_whenCalculateFare_ShouldReturnTotalFare() {
+	public void givenDistanceAndTime_whenCalculateFare_ShouldReturnTotalFare() { // Pass Case
 		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
-        double totalFare = invoiceService.calculateFare(2.0, 5);
-        System.out.println("Total Fare = " + totalFare);
-        Assert.assertEquals(25.0, totalFare, 0.0);
-    }
+		double totalFare = invoiceService.calculateFare(2.0, 5);
+		System.out.println("Total Fare = " + totalFare);
+		Assert.assertEquals(25.0, totalFare, 0.0);
+	}
+
+	@Test
+	public void givenDistanceAndTime1_whenCalculateFare_ShouldReturnTotalFare() { // Fail Case
+		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+		double totalFare = invoiceService.calculateFare(0, 5);
+		System.out.println("Total Fare = " + totalFare);
+		Assert.assertEquals(25.0, totalFare, 0.0);
+	}
+
+	@Test
+	public void givenDistanceAndTime_whenCalculateFareIsLessThan5_ShouldReturnTotalFareAs5() {
+		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+		double totalFare = invoiceService.calculateFare(0.1, 1);
+		System.out.println("Total Fare = " + totalFare);
+		Assert.assertEquals(5, totalFare, 0.0);
+	}
 }
